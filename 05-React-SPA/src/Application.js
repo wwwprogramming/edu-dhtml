318546b794dfc230813cc77fcc3a6fc5a51add98
Hannu L <hannu@example.com>
2016-06-03 23:28:27 +0300

    more uncomplete stuff.

--- 05-React-SPA/src/Application.js
+++ 05-React-SPA/src/Application.js
@@ -1,30 +1,46 @@
 import React from 'react';
-import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory, withRouter } from 'react-router'
 
 
 
 import Header from './Header';
 import Footer from './Footer';
+
 import About from './About';
-import Home from './Home';
+
+import ListEntries from './ListEntries';
+import CreateForm from './CreateForm';
+import UpdateForm from './UpdateForm';
+import Entry from './Entry';
+import Layout from './Layout';
+// var moment = require('moment') < - - will be this after es6 to 5 transpiling
+import moment from "moment";
 
 
 
 export default class Application extends React.Component {
   
   render() {
+    var divStyle = {
+    minHeight: '300px',
+    backgroundColor: '#F9F9F9'
+    };
     return (
-    <div>
-    <Header />  
-    <p>application dataaa</p>
-    <Router history={browserHistory}>
-    	<Route path="/sap.html">
-    		
-    		<IndexRoute component={Home} /> 
-    	</Route>
-    	<Route path="/about" component={ About } />
-    </Router>
-    <Footer />
+    <div style={divStyle}>
+        <Header />  
+        <p>This is just a example to show CRUD + List application UI created with React / React Router / jQuery</p>
+        <Router history={browserHistory}>
+        	
+            <Route path="/" component={Layout} >
+                <IndexRoute rows={[{id:1, title: "title-1", start:moment(), end: moment()}]} component={ListEntries} />
+                <Route path="/about" component={ About } />
+                <Route path="/create" component={ CreateForm } />
+                <Route path="/edit/:id" component={ withRouter(UpdateForm) } />
+                <Route path="/show/:id" component={ Entry } />
+            </Route>
+                
+        </Router>
+        <Footer />
     </div>);
   }
 }
